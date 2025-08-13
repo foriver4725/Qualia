2a8485a8a21dee9be6c32b984e7b169f0a06ad84
foriver4725 <hrt67@icloud.com>
2025-08-14 03:17:29 +0900

    ウィンドウサイズ小さくした、EnvオブジェクトをStaticに

--- Assets/_MyAssets/Scripts/Common/RuntimeSettingsInitializer.cs
+++ Assets/_MyAssets/Scripts/Common/RuntimeSettingsInitializer.cs
@@ -5,7 +5,7 @@ internal static class RuntimeSettingsInitializer
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Init()
     {
-        Screen.SetResolution(1920, 1080, true);
+        Screen.SetResolution(1536, 864, true);
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 30;
     }
