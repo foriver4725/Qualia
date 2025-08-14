89c3893db426f0bcfe797445558eab07caa2bb33
foriver4725 <hrt67@icloud.com>
2025-08-14 09:24:58 +0900

    フルスクリーンはオフに

--- Assets/_MyAssets/Scripts/Common/RuntimeSettingsInitializer.cs
+++ Assets/_MyAssets/Scripts/Common/RuntimeSettingsInitializer.cs
@@ -5,7 +5,7 @@ internal static class RuntimeSettingsInitializer
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Init()
     {
-        Screen.SetResolution(1536, 864, true);
+        Screen.SetResolution(1536, 864, false);
         QualitySettings.vSyncCount = 0;
         Application.targetFrameRate = 30;
     }
