046f09ef985d20a3b54372725c265d191bb576d8
foriver4725 <hrt67@icloud.com>
2025-08-15 20:48:18 +0900

    プレイヤーのコントロール基盤、マイスクリプトに移行

--- Assets/_MyAssets/Scripts/Common/InputManager.cs
+++ Assets/_MyAssets/Scripts/Common/InputManager.cs
@@ -2,6 +2,12 @@
 {
     internal sealed class InputManager : AInputManager<InputManager>
     {
+        // PlayerControl
+        internal InputInfo PcMove { get; private set; }
+        internal InputInfo PcLook { get; private set; }
+        internal InputInfo PcJump { get; private set; }
+        internal InputInfo PcSprint { get; private set; }
+
         // InGame
         internal InputInfo InGameSubmit { get; private set; }
         internal InputInfo InGameCancel { get; private set; }
@@ -12,6 +18,11 @@
 
         private protected sealed override void Init()
         {
+            PcMove = Setup(_ia.PlayerControl.Move, InputType.Value2);
+            PcLook = Setup(_ia.PlayerControl.Look, InputType.Value2);
+            PcJump = Setup(_ia.PlayerControl.Jump, InputType.Click);
+            PcSprint = Setup(_ia.PlayerControl.Sprint, InputType.Value0);
+
             InGameSubmit = Setup(_ia.InGame.Submit, InputType.Click);
             InGameCancel = Setup(_ia.InGame.Cancel, InputType.Click);
             InGameTriggerCharacter = Setup(_ia.InGame.TriggerCharacter, InputType.Click);
