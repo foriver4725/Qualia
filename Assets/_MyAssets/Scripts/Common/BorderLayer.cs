07ea08d8d5d5b4a5c2ee49b48d76935343ff7420
foriver4725 <hrt67@icloud.com>
2025-09-02 04:25:06 +0900

    ４種類のBorderVariant作成

--- Assets/_MyAssets/Scripts/Common/BorderLayer.cs
+++ Assets/_MyAssets/Scripts/Common/BorderLayer.cs
@@ -2,5 +2,11 @@ namespace MyScripts.Common;
 
 internal static class BorderLayer
 {
-    internal static readonly byte WalkSound = 10;
+    internal static class WalkSound
+    {
+        internal static readonly byte Grass = 10;
+        internal static readonly byte Sand = 11;
+        internal static readonly byte Rock = 12;
+        internal static readonly byte Water = 13;
+    }
 }
