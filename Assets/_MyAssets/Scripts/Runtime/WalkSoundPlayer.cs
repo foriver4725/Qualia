c6b33495d044dbba2698423a6442b579cbeabc29
foriver4725 <hrt67@icloud.com>
2025-09-02 06:36:31 +0900

    シンプルなサウンド再生処理を作成

--- Assets/_MyAssets/Scripts/Runtime/WalkSoundPlayer.cs
+++ Assets/_MyAssets/Scripts/Runtime/WalkSoundPlayer.cs
@@ -9,27 +9,79 @@ namespace MyScripts.Runtime
         [SerializeField, Range(1, 8), Tooltip("同時に鳴る足音の最大数")] private byte maxSoundAmount = 4;
 
         private AudioSource[] audioSources = null;
+        private bool[] arePlaying = null;
+        private int headIndex = 0; // 順番にサウンドを鳴らし、まだ鳴っていないものの先頭を指し示す
+
+        private SWalkSound.Surface currentSurface = SWalkSound.Surface.None;
 
         private void Awake()
         {
+            InitializeAudioSources();
+        }
+
+        /// <summary>
+        /// プレイヤーの地面の、更新通知を送る
+        /// </summary>
+        internal void LetPlay(SWalkSound.Surface surface)
+        {
+            if (surface == currentSurface) return;
+            currentSurface = surface;
+
+            // TODO: これらの非同期処理が混線すると？
+            if (currentSurface == SWalkSound.Surface.None)
+            {
+                // 現在鳴っているサウンドをフェードアウトして止める
+
+                for (int i = 0; i < (headIndex - 1); i++)
+                {
+                    audioSources[i]
+                        .DOFade(0.0f, 0.2f)
+                        .SetEase(Ease.OutQuad)
+                        .OnComplete(() =>
+                        {
+                            audioSources[i].Stop();
+                            audioSources[i].clip = null;
+                            arePlaying[i] = false;
+
+                            headIndex = 0;
+                        });
+                }
+            }
+            else
+            {
+                // 新規にサウンドの再生を始める
+
+                AudioClip clip = walkSoundRef.GetClip(currentSurface);
+                if (clip == null)
+                {
+                    "Failed to get walk sound clip.".LogWarning();
+                    return;
+                }
+
+                audioSources[headIndex].clip = clip;
+                audioSources[headIndex].Play();
+
+                headIndex++;
+            }
+        }
+
+        private void InitializeAudioSources()
+        {
+            audioSources = new AudioSource[maxSoundAmount];
+            arePlaying = new bool[maxSoundAmount];
+
             for (int i = 0; i < maxSoundAmount; i++)
             {
                 AudioSource source = walkSoundRoot.gameObject.AddComponent<AudioSource>();
                 source.outputAudioMixerGroup = walkSoundRef.Group;
                 source.playOnAwake = false;
-                source.loop = false;
+                source.loop = true;
+
+                audioSources[i] = source;
+                arePlaying[i] = false;
             }
-        }
 
-        /// <summary>
-        /// プレイヤーがいる地面に対応する足音を鳴らす
-        /// </summary>
-        internal void LetPlay(SWalkSound.Surface surface)
-        {
-            AudioClip clip = walkSoundRef.GetClip(surface);
-
-            //TODO: 音を鳴らす/止める
-            surface.Log();
+            headIndex = 0;
         }
     }
 }
