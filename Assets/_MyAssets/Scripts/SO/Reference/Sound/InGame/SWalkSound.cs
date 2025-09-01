42009f0f2d873f46b003b7853fa4d43265d2c073
foriver4725 <hrt67@icloud.com>
2025-09-02 04:04:29 +0900

    SOを作って設定

---
+++ Assets/_MyAssets/Scripts/SO/Reference/Sound/InGame/SWalkSound.cs
@@ -0,0 +1,28 @@
+namespace MyScripts.SO.Reference
+{
+    [CreateAssetMenu(fileName = "_WalkSound", menuName = "SO/Reference/WalkSound")]
+    internal sealed class SWalkSound : ScriptableObject
+    {
+        [SerializeField] private AudioClip grass;
+        [SerializeField] private AudioClip sand;
+        [SerializeField] private AudioClip rock;
+        [SerializeField] private AudioClip water;
+
+        internal enum Surface : byte
+        {
+            Grass,
+            Sand,
+            Rock,
+            Water,
+        }
+
+        internal AudioClip GetClip(Surface surface) => surface switch
+        {
+            Surface.Grass => grass,
+            Surface.Sand => sand,
+            Surface.Rock => rock,
+            Surface.Water => water,
+            _ => throw new ArgumentOutOfRangeException(nameof(surface), surface, null)
+        };
+    }
+}
