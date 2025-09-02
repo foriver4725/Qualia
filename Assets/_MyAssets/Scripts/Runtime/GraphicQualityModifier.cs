2cba49aaa80f04d8a8a29e8c8a8dd4e268d71462
foriver4725 <hrt67@icloud.com>
2025-09-02 20:59:53 +0900

    スクリプト作成

---
+++ Assets/_MyAssets/Scripts/Runtime/GraphicQualityModifier.cs
@@ -0,0 +1,33 @@
+using UnityEngine.Rendering;
+
+namespace MyScripts.Runtime
+{
+    internal sealed class GraphicQualityModifier : MonoBehaviour
+    {
+        [SerializeField] private RenderPipelineAsset low;
+        [SerializeField] private RenderPipelineAsset medium;
+        [SerializeField] private RenderPipelineAsset high;
+
+        private void Update()
+        {
+            if (InputManager.DebugSetGraphicQualityLow.Bool)
+            {
+                UpdateRPAsset(low);
+            }
+            else if (InputManager.DebugSetGraphicQualityMedium.Bool)
+            {
+                UpdateRPAsset(medium);
+            }
+            else if (InputManager.DebugSetGraphicQualityHigh.Bool)
+            {
+                UpdateRPAsset(high);
+            }
+        }
+
+        private static void UpdateRPAsset(RenderPipelineAsset asset)
+        {
+            if (GraphicsSettings.defaultRenderPipeline != asset)
+                GraphicsSettings.defaultRenderPipeline = asset;
+        }
+    }
+}
