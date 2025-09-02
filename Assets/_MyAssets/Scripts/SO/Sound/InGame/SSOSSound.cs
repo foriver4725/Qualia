be2be17d916cfe05add9b9c2423652eb7396fbec
foriver4725 <hrt67@icloud.com>
2025-09-02 15:39:05 +0900

    SOスクリプト作成

---
+++ Assets/_MyAssets/Scripts/SO/Sound/InGame/SSOSSound.cs
@@ -0,0 +1,22 @@
+namespace MyScripts.SO
+{
+    [CreateAssetMenu(fileName = "_SOSSound", menuName = "SO/Sound/InGame/SOS Sound")]
+    internal sealed class SSOSSound : ASSoundWithType<SSOSSound.Situation>
+    {
+        [SerializeField] private AudioClip couldRemove;
+        [SerializeField] private AudioClip couldNotRemove;
+
+        internal enum Situation : byte
+        {
+            CouldRemove,
+            CouldNotRemove,
+        }
+
+        internal sealed override AudioClip GetClip(Situation situation) => situation switch
+        {
+            Situation.CouldRemove => couldRemove,
+            Situation.CouldNotRemove => couldNotRemove,
+            _ => throw new ArgumentOutOfRangeException(nameof(situation), situation, null)
+        };
+    }
+}
