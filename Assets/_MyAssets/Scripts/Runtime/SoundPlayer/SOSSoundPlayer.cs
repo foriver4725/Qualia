6776ae8e5499fa04cbbaddfe95f8d7a2e7fee1e9
foriver4725 <hrt67@icloud.com>
2025-09-02 17:34:32 +0900

    SoundPlayerを抽象化

--- Assets/_MyAssets/Scripts/Runtime/SoundPlayer/SOSSoundPlayer.cs
+++ Assets/_MyAssets/Scripts/Runtime/SoundPlayer/SOSSoundPlayer.cs
@@ -2,40 +2,9 @@ namespace MyScripts.Runtime
 {
     internal sealed class SOSSoundPlayer : ASoundPlayerWithType<SSOSSound, SSOSSound.Situation>
     {
-        private AudioSource[] audioSources = null;
-
-        internal sealed override void LetPlay(SSOSSound.Situation type)
-        {
-            AudioClip clip = Param.GetClip(type);
-            if (clip == null)
-            {
-                "No valid clip exists to play.".LogWarning();
-                return;
-            }
-
-            audioSources[(byte)type].LetPlay
-            (
-                clip,
-                volume: Param.Volume
-            );
-        }
-
-        private protected sealed override void Init()
-        {
-            byte soundAmount = (byte)SSOSSound.Situation.Count;
-
-            audioSources = new AudioSource[soundAmount];
-
-            for (int i = 0; i < soundAmount; i++)
-            {
-                AudioSource source = Root.gameObject.AddComponent<AudioSource>();
-                source.LetInit
-                (
-                    Param.Group
-                );
-
-                audioSources[i] = source;
-            }
-        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private protected sealed override byte TypeToByte(SSOSSound.Situation type) => (byte)type;
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private protected sealed override byte GetTypeAmount() => (byte)SSOSSound.Situation.Count;
     }
 }
