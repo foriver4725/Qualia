c395a97ab676cc9defa4b4b8c27ab5677d8f5d78
foriver4725 <hrt67@icloud.com>
2025-09-23 00:41:17 +0900

    キャラクターの隠れているところも虹色に光るようにした。 ただ、Ｚファイティングが起こっているのと、草で隠れている部分は表示されない。

---
+++ Assets/_MyAssets/Shaders/CharacterOutline/Common/FragmentShader.hlsl
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "Assets/_MyAssets/Shaders/CharacterOutline/Common/RGB2HSV.hlsl"
+#include "Assets/_MyAssets/Shaders/CharacterOutline/Common/Parameter.hlsl"
+
+half4 frag(v2f _) : SV_Target
+{
+    half3 hsv = RGB2HSV(_BeginColor);
+    hsv.x = frac(hsv.x + _ColorChangeSpeed * _Time.y); // 経過時間でHueを進める(負値なら逆回転)
+    half3 rgb = HSV2RGB(hsv);
+    return half4(rgb, 1.0);
+}
