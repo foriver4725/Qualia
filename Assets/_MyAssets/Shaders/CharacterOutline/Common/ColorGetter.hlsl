d0eb27dd6f400036af98c9b9f3261f64f7bb7e95
foriver4725 <hrt67@icloud.com>
2025-09-24 00:48:06 +0900

    時間を入れて色を返すようにした

--- Assets/_MyAssets/Shaders/CharacterOutline/Common/ColorGetter.hlsl
+++ Assets/_MyAssets/Shaders/CharacterOutline/Common/ColorGetter.hlsl
@@ -3,10 +3,10 @@
 #include "Assets/_MyAssets/Shaders/CharacterOutline/Common/RGB2HSV.hlsl"
 #include "Assets/_MyAssets/Shaders/CharacterOutline/Common/Parameter.hlsl"
 
-half4 GetNowColor()
+half4 GetNowColor(half elapsedTime)
 {
     half3 hsv = RGB2HSV(_BeginColor);
-    hsv.x = frac(hsv.x + _ColorChangeSpeed * _Time.y); // 経過時間でHueを進める(負値なら逆回転)
+    hsv.x = frac(hsv.x + _ColorChangeSpeed * elapsedTime); // 経過時間でHueを進める(負値なら逆回転)
     half3 rgb = HSV2RGB(hsv);
     return half4(rgb, 1.0);
 }
