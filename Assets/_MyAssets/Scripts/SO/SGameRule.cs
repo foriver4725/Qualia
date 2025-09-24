e35f2a2a91c488ee051a85d03817a4c7fc4f2066
foriver4725 <hrt67@icloud.com>
2025-09-24 18:38:51 +0900

    災害の発生・終了条件を、SOで作成

--- Assets/_MyAssets/Scripts/SO/SGameRule.cs
+++ Assets/_MyAssets/Scripts/SO/SGameRule.cs
@@ -3,38 +3,50 @@ namespace MyScripts.SO
     [CreateAssetMenu(fileName = "_GameRule", menuName = "SO/Game Rule")]
     internal sealed class SGameRule : ScriptableObject
     {
-        internal interface IClearCondition
-        {
-            // 〇秒経過でクリア
-            float ShouldElapse { get; }
-            // 最大値
-            float MaxElapse { get; }
-
-            // 〇個除去でクリア
-            byte ShouldFind { get; }
-            // 最大値
-            byte MaxFind { get; }
-        }
-
         [Serializable]
-        internal sealed class ClearCondition : IClearCondition
+        internal sealed class ClearCondition
         {
             [SerializeField, Range(0.0f, 1.0e4f), Header("〇秒あるうち、")] private float maxElapse = 600.0f;
             [SerializeField, Range(0.0f, 1.0e4f), Header("〇秒経過でクリア (一緒の数値のはず)")] private float shouldElapse = 600.0f;
+            internal float MaxElapse => maxElapse;
+            internal float ShouldElapse => shouldElapse;
+
             [Space(30)]
+
             [SerializeField, Range(0, 50), Header("〇個あるうち、")] private byte maxFind = 10;
             [SerializeField, Range(0, 50), Header("〇個除去でクリア")] private byte shouldFind = 5;
+            internal byte MaxFind => maxFind;
+            internal byte ShouldFind => shouldFind;
+        }
 
-            public float ShouldElapse => shouldElapse;
-            public float MaxElapse => maxElapse;
+        [Serializable]
+        internal sealed class DisasterOccurrenceConditions
+        {
+            [Serializable]
+            internal sealed class SingleCondition
+            {
+                [SerializeField, Header("発生する災害")] private Disaster disaster = Disaster.None;
+                internal Disaster Disaster => disaster;
 
-            public byte ShouldFind => shouldFind;
-            public byte MaxFind => maxFind;
+                [Space(10)]
+
+                [SerializeField, Range(0, 50), Header("〇個目を除去したら発生し、")] private byte beginCount = 2;
+                [SerializeField, Range(0, 50), Header("△個目を除去するまたは、")] private byte endCount = 3;
+                [SerializeField, Range(0.0f, 1.0e4f), Header("□秒経過したら終了する")] private float endDuration = 90.0f;
+                internal byte BeginCount => beginCount;
+                internal byte EndCount => endCount;
+                internal float EndDuration => endDuration;
+            }
+
+            [SerializeField, Header("発生順に設定すること")] private SingleCondition[] conditions = Array.Empty<SingleCondition>();
+            internal IReadOnlyList<SingleCondition> Conditions => conditions;
         }
 
         //　今後、複数ステージに増える予定
         [SerializeField] private ClearCondition clearCondition;
+        [SerializeField] private DisasterOccurrenceConditions disasterOccurrenceConditions;
 
-        internal IClearCondition GetClearCondition() => clearCondition;
+        internal ClearCondition GetClearCondition() => clearCondition;
+        internal DisasterOccurrenceConditions GetDisasterOccurrenceConditions() => disasterOccurrenceConditions;
     }
 }
