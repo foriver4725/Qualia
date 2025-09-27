00b3e4d2450379f62b8b5213151d0defe00336a2
foriver4725 <hrt67@icloud.com>
2025-09-27 19:07:23 +0900

    サウンド追加・SO設定

---
+++ Assets/_MyAssets/Scripts/SO/InGame/Sound/SDisasterSound.cs
@@ -0,0 +1,24 @@
+namespace MyScripts.SO
+{
+    [CreateAssetMenu(fileName = "_DisasterSound", menuName = "SO/Sound/InGame/Disaster")]
+    internal sealed class SDisasterSound : ASSoundWithType<SDisasterSound.Disaster>
+    {
+        [SerializeField] private AudioClip windstorm;
+        [SerializeField] private AudioClip blizzard;
+
+        internal enum Disaster : byte
+        {
+            Windstorm,
+            Blizzard,
+
+            Count,
+        }
+
+        internal sealed override AudioClip GetClip(Disaster type) => type switch
+        {
+            Disaster.Windstorm => windstorm,
+            Disaster.Blizzard => blizzard,
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+    }
+}
