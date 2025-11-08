0506849c86860546492b8f880afb0676dfc5d34c
foriver4725 <hrt67@icloud.com>
2025-11-08 23:14:04 +0900

    SOを設定

---
+++ Assets/_MyAssets/Scripts/SO/Sound/OutGame/SButton.cs
@@ -0,0 +1,24 @@
+﻿namespace MyScripts.SO
+{
+    [CreateAssetMenu(fileName = "_Button", menuName = "SO/Sound/OutGame/Button")]
+    internal sealed class SButton : ASSoundWithType<SButton.Action>
+    {
+        [SerializeField] private AudioClip hover;
+        [SerializeField] private AudioClip click;
+
+        internal enum Action : byte
+        {
+            Hover,
+            Click,
+
+            Count,
+        }
+
+        internal sealed override AudioClip GetClip(Action type) => type switch
+        {
+            Action.Hover => hover,
+            Action.Click => click,
+            _            => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+    }
+}
