e8dd8f47c5380b5e4cc6c7b99a56344a15986a44
foriver4725 <hrt67@icloud.com>
2025-11-08 19:39:21 +0900

    共通具象クラスとして作る

---
+++ Assets/_MyAssets/Scripts/Runtime/Button/SceneChangeButtonManager.cs
@@ -0,0 +1,54 @@
+using MyScripts.Common.Button;
+
+namespace MyScripts.Runtime
+{
+    internal sealed class SceneChangeButtonManager :  ATextButtonManager
+    {
+        [SerializeField] private Scene targetScene;
+
+        private bool isClickEnabled = true;
+
+        // どれか一つが押されたら、他のボタンは無効になる
+        // 再度有効になることはない
+        private static SceneChangeButtonManager[] linkedButtons = null;
+
+        private protected sealed override void OnJustAfterAwake()
+        {
+            if (linkedButtons == null)
+                linkedButtons = FindObjectsByType<SceneChangeButtonManager>(
+                    FindObjectsInactive.Include, FindObjectsSortMode.None);
+        }
+
+        private void OnDestroy()
+        {
+            if (linkedButtons != null)
+                linkedButtons = null;
+        }
+
+        private protected sealed override void OnClickSucceeded()
+        {
+            SetLinkedButtonsClicked();
+            LoadManager.Instance.BeginLoad(targetScene);
+        }
+
+        private protected sealed override void PlayHoverSe()
+        {
+        }
+
+        private protected sealed override void PlayClickSe()
+        {
+        }
+
+        private void SetLinkedButtonsClicked()
+        {
+            if (!isClickEnabled) return;
+            if (linkedButtons == null) return;
+
+            foreach (var linkedButton in linkedButtons)
+            {
+                if (linkedButton == null) continue;
+                linkedButton.isClickEnabled = false;
+            }
+        }
+    }
+}
--- Assets/_MyAssets/Scripts/Runtime/Button/TitleStartButtonManager.cs
+++
@@ -1,9 +0,0 @@
-using MyScripts.Common.Button;
-
-namespace MyScripts.Runtime
-{
-    internal sealed class TitleStartButtonManager : ASceneChangeButtonManager
-    {
-        private protected sealed override Scene TargetScene => Scene.Select;
-    }
-}
