6309e343d65eca441fef0e659fd39fa350801188
foriver4725 <hrt67@icloud.com>
2025-12-06 00:16:17 +0900

    デフォルトの選択なし、NewとContinueも選択ボタンにした

--- Assets/_MyAssets/Scripts/Runtime/UI/Title/SaveSlot/StartOption/ContinueButtonManager.cs
+++ Assets/_MyAssets/Scripts/Runtime/UI/Title/SaveSlot/StartOption/ContinueButtonManager.cs
@@ -2,8 +2,18 @@ using MyScripts.Runtime.UI.Button;
 
 namespace MyScripts.Runtime.UI.Title.SaveSlot.StartOption
 {
-    internal sealed class ContinueButtonManager : AButtonManager
+    internal sealed class ContinueButtonManager : ASelectableButtonWithFrameManager
     {
+        private protected sealed override bool IsFrontUI =>
+           UIActivationManager.Instance.Front == UIActivationManager.UI.SaveSlot
+           && StateRootObjectManager.Instance.State == State.StartOption;
+
+        private protected sealed override void OnSubmittedWithSelection()
+        {
+            base.PlayClickSe();
+            this.OnClickSucceeded();
+        }
+
         private protected sealed override void OnClickSucceeded()
         {
             base.OnClickSucceeded();
