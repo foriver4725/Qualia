0cab121940ca7ee01870752b499c198f01b1f8c6
foriver4725 <hrt67@icloud.com>
2025-12-05 22:37:30 +0900

    SlotManager作成

--- Assets/_MyAssets/Scripts/Runtime/UI/Title/SaveSlot/Select/SlotManager.cs
+++ Assets/_MyAssets/Scripts/Runtime/UI/Title/SaveSlot/Select/SlotManager.cs
@@ -2,12 +2,19 @@ namespace MyScripts.Runtime.UI.Title.SaveSlot.Select
 {
     internal sealed class SlotManager : MonoBehaviour
     {
+        [SerializeField] private Image image;
         [SerializeField, Range(0, 10)] private int slotIndex = 0;
 
-        private void OnClicked()
+        private void Awake()
         {
-            StartSettings.SlotIndex = slotIndex;
-            StateRootObjectManager.Instance.ChangeState(State.StartOption);
+            image.OnPointerDownAsObservable()
+                .Select(slotIndex, static (_, slotIndex) => slotIndex)
+                .Subscribe(static slotIndex =>
+                {
+                    StartSettings.SlotIndex = slotIndex;
+                    StateRootObjectManager.Instance.ChangeState(State.StartOption);
+                })
+                .AddTo(image);
         }
     }
 }
