10c253382c82a378a83d5f9a18d4d7fe8c385a93
foriver4725 <hrt67@icloud.com>
2025-12-05 22:34:06 +0900

    StartOptionのViewConstructor

--- Assets/_MyAssets/Scripts/Runtime/UI/Title/SaveSlot/StartOption/ViewConstructor.cs
+++ Assets/_MyAssets/Scripts/Runtime/UI/Title/SaveSlot/StartOption/ViewConstructor.cs
@@ -1,13 +1,19 @@
+using MyScripts.Common.SaveSystem;
+
 namespace MyScripts.Runtime.UI.Title.SaveSlot.StartOption
 {
     // UIが有効になるたびに実行するべき
     // 現在の数値を基に、見た目を再構成する
     internal sealed class ViewConstructor : MonoBehaviour
     {
+        [SerializeField] private ContinueButtonManager continueButtonManager;
+
         internal void Construct()
         {
-            // Continueボタンを消したり
-            throw new NotImplementedException();
+            if (SaveLoadManager.Data.Slots[StartSettings.SlotIndex].IsValid)
+                continueButtonManager.Parent.gameObject.SetActive(true);
+            else
+                continueButtonManager.Parent.gameObject.SetActive(false);
         }
     }
 }
