00c88ca1f0e6c9f306a098a8bd08799228ceb590
foriver4725 <hrt67@icloud.com>
2025-12-08 15:14:55 +0900

    ボタンフレームのオンオフ設定を追加

---
+++ Assets/_MyAssets/Scripts/SO/OutGame/SGameConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace MyScripts.SO
+{
+    [CreateAssetMenu(fileName = "_GameConfig", menuName = "SO/OutGame/Game Config")]
+    internal sealed class SGameConfig : ScriptableObject
+    {
+        [SerializeField] private bool doesAlwaysShowGamepadButtonFrame = false;
+        internal bool DoesAlwaysShowGamepadButtonFrame => doesAlwaysShowGamepadButtonFrame;
+    }
+}
