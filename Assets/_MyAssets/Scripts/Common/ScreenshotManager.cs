2c8ed49b202f958bb741a72ca4efa9f22296c6f5
foriver4725 <hrt67@icloud.com>
2025-12-08 16:46:28 +0900

    一応1フレーム待つ

--- Assets/_MyAssets/Scripts/Common/ScreenshotManager.cs
+++ Assets/_MyAssets/Scripts/Common/ScreenshotManager.cs
@@ -43,6 +43,7 @@ namespace MyScripts.Common
                 playerCamera.transform.position,
                 playerCamera.transform.rotation
             );
+            await UniTask.NextFrame(); // カメラ位置反映のため1フレーム待つ
             renderingCamera.Render();
 
             "Screenshot captured".Print();
