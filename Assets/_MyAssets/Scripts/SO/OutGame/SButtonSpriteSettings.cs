828991aac4914f0f046b887263b16a8dd1680d47
foriver4725 <hrt67@icloud.com>
2025-12-08 14:36:40 +0900

    参照を設定しなおし

--- Assets/_MyAssets/Scripts/SO/OutGame/SButtonSpriteSettings.cs
+++ Assets/_MyAssets/Scripts/SO/OutGame/SButtonSpriteSettings.cs
@@ -9,15 +9,26 @@ namespace MyScripts.SO
         {
             Normal,
             SaveSlot,
+
+            Pause_Resume,
+            Pause_BackToTitle,
+            Pause_BackToDesktop,
         }
 
         [SerializeField] private SpriteSettings normal;
         [SerializeField] private SpriteSettings saveSlot;
+        [Space(10)]
+        [SerializeField] private SpriteSettings pause_Resume;
+        [SerializeField] private SpriteSettings pause_BackToTitle;
+        [SerializeField] private SpriteSettings pause_BackToDesktop;
 
         internal SpriteSettings Get(ButtonType type) => type switch
         {
             ButtonType.Normal => normal,
             ButtonType.SaveSlot => saveSlot,
+            ButtonType.Pause_Resume => pause_Resume,
+            ButtonType.Pause_BackToTitle => pause_BackToTitle,
+            ButtonType.Pause_BackToDesktop => pause_BackToDesktop,
             _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
         };
     }
