7f89123a491d3efc1b88261ec917c35521a7432d
foriver4725 <hrt67@icloud.com>
2026-01-07 10:38:03 +0900

    スクリプト作成

--- Assets/_MyAssets/Scripts/Runtime/InGameSOHolder.cs
+++ Assets/_MyAssets/Scripts/Runtime/InGameSOHolder.cs
@@ -5,9 +5,11 @@
         [SerializeField] private SGameParameter gameParameter;
         [SerializeField] private SPlayerControl playerControl;
         [SerializeField] private SSOSSignLogText sosSignLogText;
+        [SerializeField] private SCutScene cutScene;
 
         internal SGameParameter GameParameter => gameParameter;
         internal SPlayerControl PlayerControl => playerControl;
         internal SSOSSignLogText SOSSignLogText => sosSignLogText;
+        internal SCutScene CutScene => cutScene;
     }
 }
---
+++ Assets/_MyAssets/Scripts/SO/InGame/SCutScene.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine.Video;
+
+namespace MyScripts.SO
+{
+    [CreateAssetMenu(fileName = "_CutScene", menuName = "SO/InGame/CutScene")]
+    internal sealed class SCutScene : ScriptableObject
+    {
+        internal enum CutSceneType : byte
+        {
+            Intro,         // オープニング
+            AnimaDesc,     // アニマの説明
+            SOSRemoveDesc, // SOSサイン除去の説明
+        }
+
+        [SerializeField] private VideoClip introClip;
+        [SerializeField] private VideoClip animaDescClip;
+        [SerializeField] private VideoClip sosRemoveDescClip;
+
+        internal VideoClip Get(CutSceneType type) => type switch
+        {
+            CutSceneType.Intro => introClip,
+            CutSceneType.AnimaDesc => animaDescClip,
+            CutSceneType.SOSRemoveDesc => sosRemoveDescClip,
+            _ => throw new System.ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+    }
+}
