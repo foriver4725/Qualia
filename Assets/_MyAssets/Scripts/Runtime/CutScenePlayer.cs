b56fe75e78f511828d5777dd1bf8bbc6b87d8f4c
foriver4725 <hrt67@icloud.com>
2026-01-07 11:31:02 +0900

    フェードイン・アウトして再生する

--- Assets/_MyAssets/Scripts/Runtime/CutScenePlayer.cs
+++ Assets/_MyAssets/Scripts/Runtime/CutScenePlayer.cs
@@ -4,11 +4,16 @@ namespace MyScripts.Runtime
 {
     internal sealed class CutScenePlayer : MonoBehaviour
     {
+        [SerializeField, Range(0.0f, 5.0f)] private float bgFadeDuration = 0.5f;
+        [SerializeField] private Image bg;
         [SerializeField] private RawImage rawImage;
         [SerializeField] private VideoPlayer videoPlayer;
 
         private bool isPlaying = false;
         private SCutScene.CutSceneType currentPlayingType;
+
+        // Awake で初期化
+        private float bgAlphaMax;
 
         internal bool IsPlaying => isPlaying;
 
@@ -17,7 +22,9 @@ namespace MyScripts.Runtime
             rawImage.enabled = false;
             videoPlayer.source = VideoSource.VideoClip;
 
-            3.0f.SecAwaitThenDo(() => Play(SCutScene.CutSceneType.Intro)).Forget();
+            bgAlphaMax = bg.color.a;
+            SetBgAlpha(0.0f);
+            bg.enabled = false;
         }
 
         private void OnDestroy()
@@ -84,11 +91,45 @@ namespace MyScripts.Runtime
         private void OnBeginPlay()
         {
             InputManager.DisableAllInputs();
+            FadeInBg(destroyCancellationToken).Forget();
         }
 
         private void OnEndPlay()
         {
             InputManager.EnableAllInputs();
+            FadeOutBg(destroyCancellationToken).Forget();
+        }
+
+        // 重複実行はバグると思う
+        private async UniTaskVoid FadeInBg(Ct ct)
+        {
+            SetBgAlpha(0.0f);
+            bg.enabled = true;
+
+            await LMotion.Create(0.0f, bgAlphaMax, bgFadeDuration)
+                        .WithEase(Ease.OutQuad)
+                        .Bind(SetBgAlpha)
+                        .ToUniTask(cancellationToken: ct);
+        }
+
+        // 重複実行はバグると思う
+        private async UniTaskVoid FadeOutBg(Ct ct)
+        {
+            SetBgAlpha(bgAlphaMax);
+
+            await LMotion.Create(bgAlphaMax, 0.0f, bgFadeDuration)
+                        .WithEase(Ease.InQuad)
+                        .Bind(SetBgAlpha)
+                        .ToUniTask(cancellationToken: ct);
+
+            bg.enabled = false;
+        }
+
+        private void SetBgAlpha(float alpha)
+        {
+            Color color = bg.color;
+            color.a = alpha;
+            bg.color = color;
         }
     }
 }
