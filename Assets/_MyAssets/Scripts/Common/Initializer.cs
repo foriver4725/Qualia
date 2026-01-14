a330434670b700198583fc19fa957c402ce9522e
foriver4725 <hrt67@icloud.com>
2026-01-14 12:48:08 +0900

    初期化の処理を行う

---
+++ Assets/_MyAssets/Scripts/Common/Initializer.cs
@@ -0,0 +1,23 @@
+﻿namespace MyScripts.Common;
+
+internal static class Initializer
+{
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+    private static void Init()
+    {
+        Screen.SetResolution(1920, 1080, true);
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = 30;
+
+        ConnectSteamAsync().Forget();
+    }
+
+    private static async UniTaskVoid ConnectSteamAsync()
+    {
+        // 初期化が成功するまで、リトライし続ける
+        while (!Steam.APIConnector.Init())
+        {
+            await UniTask.DelayFrame(16);
+        }
+    }
+}
