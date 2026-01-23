5148c92906ba7601dcdf222b11e037ccd3ecb88a
foriver4725 <hrt67@icloud.com>
2026-01-23 18:08:42 +0900

    共通のマテリアルを使っていたので修正

--- Assets/_MyAssets/Scripts/Runtime/PanoramaImageRotator.cs
+++ Assets/_MyAssets/Scripts/Runtime/PanoramaImageRotator.cs
@@ -11,15 +11,31 @@ namespace MyScripts.Runtime
         [SerializeField, Range(0.0f, 1.0f)] private float rotateSpeed = 0.02f; // 1秒あたりのU変化量
 
         private static readonly int MainTexST_ID = Shader.PropertyToID("_MainTex_ST");
+        private Material material = null;
+
+        private void Awake()
+        {
+            material = new Material(image.material);
+            image.material = material;
+        }
+
+        private void OnDestroy()
+        {
+            if (material != null)
+            {
+                Destroy(material);
+                material = null;
+            }
+        }
 
         private void Update()
         {
             // _MainTex_ST = (scale.x, scale.y, offset.x, offset.y)
-            Vector4 st = image.material.GetVector(MainTexST_ID);
+            Vector4 st = material.GetVector(MainTexST_ID);
 
             // offset.x が U座標のオフセットに対応
             st.z = Mathf.Repeat(st.z + rotateSpeed * Time.deltaTime, 1.0f);
-            image.material.SetVector(MainTexST_ID, st);
+            material.SetVector(MainTexST_ID, st);
         }
     }
 }
