16fd13693f1a2ccd0fb540c6ed34d99e7b90058f
foriver4725 <hrt67@icloud.com>
2026-01-25 23:43:01 +0900

    SOもファイルで統一

---
+++ Assets/_MyAssets/Scripts/SO/OutGame/SCloudFileUrl.cs
@@ -0,0 +1,33 @@
+﻿namespace MyScripts.SO
+{
+    [CreateAssetMenu(fileName = "_CloudFileUrl", menuName = "SO/OutGame/Cloud File Url")]
+    internal sealed class SCloudFileUrl : ScriptableObject
+    {
+        [Header("画像")]
+        [SerializeField] private string image_empty;
+        [Space(10)]
+
+        [Header("動画")]
+        [SerializeField] private string movie_intro;
+        [Space(10)]
+
+        [Header("サウンド")]
+        [SerializeField] private string sound_empty;
+        [Space(10)]
+
+        [Header("テキスト")]
+        [SerializeField] private string text_empty;
+        // [Space(10)]
+
+        internal enum FileType : byte
+        {
+            Movie_Intro, // 導入 & チュートリアル
+        }
+
+        internal string Get(FileType type) => type switch
+        {
+            FileType.Movie_Intro => movie_intro,
+            _ => throw new ArgumentOutOfRangeException(nameof(type), type, null)
+        };
+    }
+}
