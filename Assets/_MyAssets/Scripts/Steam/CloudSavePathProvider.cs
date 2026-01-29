cc67ba213b4617b8dbec3f8b028037d1e561d09b
foriver4725 <hrt67@icloud.com>
2026-01-30 06:25:04 +0900

    クラウドセーブのパスに保存されるようにした

--- Assets/_MyAssets/Scripts/Steam/CloudSavePathProvider.cs
+++ Assets/_MyAssets/Scripts/Steam/CloudSavePathProvider.cs
@@ -6,17 +6,16 @@ namespace MyScripts.Steam
     public static class CloudSavePathProvider
     {
         /// <summary>
-        /// <para>Steam Cloud のセーブデータ保存先パスを取得する</para>
+        /// <para>Steam Cloud のセーブデータ保存先ディレクトリパスを生成する</para>
         /// <para>現在のユーザーIDごとにディレクトリが分かれるようになっている</para>
         /// </summary>
         /// <param name="persistentDataPath">Application.persistentDataPath = %UserProfile%/AppData/LocalLow/[CompanyName]/[ProductName]</param>
-        /// <param name="fileName">セーブデータのファイル名 (拡張子なし)</param>
         /// <returns>%UserProfile%/AppData/LocalLow/[CompanyName]/[ProductName]/SaveData/Steam/[SteamUserID]</returns>
-        public static string Create(string persistentDataPath, string fileName)
+        public static string CreateDirectoryPath(string persistentDataPath)
         {
             // 64bit
             ulong steamUserId = SteamUser.GetSteamID().m_SteamID;
-            return Path.Combine(persistentDataPath, "SaveData", "Steam", steamUserId.ToString(), fileName + ".dat");
+            return Path.Combine(persistentDataPath, "SaveData", "Steam", steamUserId.ToString());
         }
     }
 }
