5633b52133012ba57cc981b456092208bcc85fb9
msyn-yamada <ya1359@icloud.com>
2026-04-14 13:43:28 +0900

    Tooltipの説明文の変更

--- Assets/_MyAssets/Scripts/SO/InGame/SPlayerControl.cs
+++ Assets/_MyAssets/Scripts/SO/InGame/SPlayerControl.cs
@@ -25,7 +25,7 @@
         [SerializeField, Tooltip("移動入力を鈍感にするタイミング")]
         private MoveInputInsensitiveTimingType moveInputInsensitiveTiming = MoveInputInsensitiveTimingType.Never;
         [SerializeField, Tooltip("移動入力を鈍感にするとき,元の入力値の何倍にするか")] private float moveInputInsensitiveRate = 0.5f;
-        [SerializeField, Tooltip("水のアニマ取得時に雨と水辺の速度倍率を併用するか") ] private bool moveSpeedIncreaseOverlapWhenHasSea;
+        [SerializeField, Tooltip("水のアニマ取得時に雨と水辺の速度加算を併用するか") ] private bool moveSpeedIncreaseOverlapWhenHasSea;
         internal float MoveSpeed => moveSpeed;
         internal float MoveSpeedIncreaseWhenHasLand => moveSpeedIncreaseWhenHasLand;
         internal float MoveSpeedIncreaseWhenHasSea => moveSpeedIncreaseWhenHasSea;
