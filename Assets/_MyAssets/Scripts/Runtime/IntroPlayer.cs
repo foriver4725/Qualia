7b629023ee49b3f48fd8656612b8696a88f0b397
foriver4725 <hrt67@icloud.com>
2026-04-18 20:59:21 +0900

    リファクタ

--- Assets/_MyAssets/Scripts/Runtime/IntroPlayer.cs
+++ Assets/_MyAssets/Scripts/Runtime/IntroPlayer.cs
@@ -3,12 +3,13 @@
 namespace MyScripts.Runtime
 {
     /// <summary>
-    /// 初めてゲームを開始した際に、最初に流れるイントロ画像シーケンスを再生するクラス
+    /// 初めてゲームを開始した際に、最初に流れるイントロを再生するクラス
     /// </summary>
     internal sealed class IntroPlayer : MonoBehaviour
     {
         [SerializeField] private Sprite[] sequenceSprites;
-        [SerializeField, Range(0.0f, 10.0f)] private float durationUntilPlay = 0.5f;
+        [SerializeField, Range(0.0f, 10.0f)] private float durationUntilStoryMovie = 0.5f;
+        [SerializeField, Range(0.0f, 10.0f)] private float durationUntilTutorialImages = 1.0f;
         [SerializeField] private ImageSequencePlayerPlayOptions playOptions;
         [SerializeField] private PauseInvoker pauseInvoker;
         [SerializeField] private SStoryMovie storyMovie;
@@ -21,14 +22,20 @@ namespace MyScripts.Runtime
             if (!Variables.IsFirstPlay)
                 return;
 
-            await durationUntilPlay.SecAwait(ct: ct);
-            // ポーズでなくなるまで待機
+            await durationUntilStoryMovie.SecAwait(ct: ct);
+            await WaitUntilUnPaused(ct);
+            await CutScenePlayer.Instance.PlayAsync(storyMovie.Get(SStoryMovie.GameProgress.P0), ct);
+
+            await durationUntilTutorialImages.SecAwait(ct: ct);
+            await WaitUntilUnPaused(ct);
+            await ImageSequencePlayer.Instance.PlayAsync(sequenceSprites, playOptions, ct);
+        }
+
+        // ポーズでなくなるまで待機する
+        private async UniTask WaitUntilUnPaused(Ct ct)
+        {
             await UniTask.WaitUntil(pauseInvoker,
                 static pauseInvoker => !pauseInvoker.IsPaused, cancellationToken: ct);
-
-            // まず動画を再生し、終了後にチュートリアルシーケンスを再生
-            await CutScenePlayer.Instance.PlayAsync(storyMovie.Get(SStoryMovie.GameProgress.P0), ct);
-            await ImageSequencePlayer.Instance.PlayAsync(sequenceSprites, playOptions, ct);
         }
     }
 }
